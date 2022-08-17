ca81f603ddfcad41fc5548f6e7956c347363eb74
Souvik Das <svkds1995@gmail.com>
2022-08-17 13:10:41 +0530

    Angular app for routing and auth guard with child routing

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+  constructor(private auth: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    if (this.auth.isLoggedIn()) {
+      this.router.navigate(['admin']);
+    }
+  }
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      this.auth.login(this.loginForm.value).subscribe(
+        (result) => {
+          console.log(result);
+          this.router.navigate(['/admin']);
+        },
+        (err: Error) => {
+          alert(err.message);
+        }
+      );
+    }
+  }
+}
